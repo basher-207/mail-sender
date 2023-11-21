91cada820c04b7a1fb1f1f5537f92b5c3a8f8086
Vladyslav Diachenko <basher207vd@gmail.com>
2023-11-21 23:35:51 +0200

    Add ./server.js and ./app.js files. Install mongoose, dotenv, express npm. Connect database to server.

---
+++ server.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+require('dotenv').config();
+const app = require('./app.js');
+const PORT = process.env.PORT || 3000
+
+const dbUrl = process.env.DB_URL.replace('<password>', process.env.DB_PASSWORD);
+mongoose.connect(dbUrl).then(() => {
+  console.log('DB connection is successful');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}...`);
+});
