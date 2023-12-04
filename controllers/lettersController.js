b6684dd35e47febe7b35cdb23ff0d482f4ddb2fb
Vladyslav Diachenko <basher207vd@gmail.com>
2023-12-04 03:26:59 +0200

    create /views/pages/letters/addLetter page. Need to split it for 2 pages

--- controllers/lettersController.js
+++ controllers/lettersController.js
@@ -1,16 +1,33 @@
+const Receiver = require('../models/ReceiverModel.js');
 const Letter = require('../models/LetterModel.js');
 const ejsPath = 'pages/letters'
 
 exports.getLetters = async (req, res) => {
-  // get all letters templates with receiver information and transform it to plain JS object
-  const letterTemplates = await Letter.find().populate({
-    path: 'interjectionsPersonalized.receiver',
-    select: 'name email'
-  }).lean();
+  try {
+    // get all letters templates with receiver information and transform it to plain JS object
+    const letterTemplates = await Letter.find().populate({
+      path: 'interjectionsPersonalized.receiver',
+      select: 'name email'
+    }).lean();
 
-  res.render(ejsPath, { letterTemplates });
+    res.render(ejsPath, { letterTemplates });
+  } catch (error) {
+    res.status(500).json({
+      status: 'fail',
+      message: error.message
+    });
+  }
 };
 
-exports.getAddLetter = (req, res) => {
-  res.render(ejsPath + '/addLetter');
-}
+exports.getAddLetter = async (req, res) => {
+  try {
+    const receiversList = await Receiver.find().lean();
+
+    res.render(ejsPath + '/addLetter', { receiversList });
+  } catch (error) {
+    res.status(500).json({
+      status: 'fail',
+      message: error.message
+    });
+  }
+};
